f6544d432d4f5b642e85d561d20875e3653afe66
srtalaie <srt4721@yahoo.com>
2021-07-28 09:23:29 -0700

    built out structure created first notebook

---
+++ notebooks/jupytext_conversion/ValueOverReplacement.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: 'Python 3.7.10 64-bit (''FFDataProject'': conda)'
+#     name: python3
+# ---
+
+import os
+import sys
+src_dir = os.path.join(os.getcwd(), '..', 'src')
+sys.path.append(src_dir)
+
+
+
+

ea0598c718779fbd3c6eeb48bc41cbb1a4c82420
srtalaie <srt4721@yahoo.com>
2021-07-28 15:58:36 -0700

    setting up df's for vor

--- notebooks/jupytext_conversion/ValueOverReplacement.py
+++ notebooks/jupytext_conversion/ValueOverReplacement.py
@@ -17,11 +17,41 @@ import sys
 src_dir = os.path.join(os.getcwd(), '..', 'src')
 sys.path.append(src_dir)
 
-from utils import GLOBAL
+from utils import GLOBAL, functions
 # -
 
 import pandas as pd
 
-pd.read_csv(GLOBAL.PROJECTIONS_2021)
+proj_df = pd.read_csv(GLOBAL.PROJECTIONS_2021, index_col=0)
+
+# +
+### DO NOT RUN IF USING 2021 PROJECTIONS OR HIGHER ####
+### Section is for older projections newer ones have the scoring columns ###
+
+#Get rid of odd column if there are any[row indexer, column indexer], change 0 to 1 if there are
+proj_df = proj_df.iloc[:, 0:]
+
+#Add in Fantasy Points based on scoring type
+proj_df['FantasyPoints'] = (
+    functions.scoringCalculator(
+        proj_df['Receptions'], proj_df['ReceivingYds'], proj_df['ReceivingTD'], proj_df['FL'], proj_df['RushingYds'], proj_df['RushingTD'], proj_df['PassingYds'], proj_df['PassingTD'], proj_df['Int'], GLOBAL.HALF_PPR_SCORING
+    )
+)
+
+#Get dataframes of specific pos
+rb_proj_df = proj_df[proj_df['Pos'] == 'RB', ['Player, Team, Pos, Receptions, ReceivingYds, FL, ReceivingTD, RushingAtt, RushingYds, RushingTD, PassingAtt, PassingYds, PassingTD, Int']]
+wr_proj_df = proj_df[proj_df['Pos'] == 'WR', ['Player, Team, Pos, Receptions, ReceivingYds, FL, ReceivingTD, RushingAtt, RushingYds, RushingTD, PassingAtt, PassingYds, PassingTD, Int']]
+te_proj_df = proj_df[proj_df['Pos'] == 'TE', ['Player, Team, Pos, Receptions, ReceivingYds, FL, ReceivingTD, RushingAtt, RushingYds, RushingTD, PassingAtt, PassingYds, PassingTD, Int']]
+qb_proj_df = proj_df[proj_df['Pos'] == 'QB', ['Player, Team, Pos, Receptions, ReceivingYds, FL, ReceivingTD, RushingAtt, RushingYds, RushingTD, PassingAtt, PassingYds, PassingTD, Int']]
+# -
+
+#Get dataframes of specific pos
+rb_proj_df = proj_df[proj_df['Pos'] == 'RB']
+wr_proj_df = proj_df[proj_df['Pos'] == 'WR']
+te_proj_df = proj_df[proj_df['Pos'] == 'TE']
+qb_proj_df = proj_df[proj_df['Pos'] == 'QB']
+
+#ADP for current year and scoring format
+adp_df = pd.read_csv(GLOBAL.ADP_2021_HALF_PPR)
 
 
--- src/utils/GLOBAL.py
+++ src/utils/GLOBAL.py
@@ -5,5 +5,56 @@ sys.path.append(src_dir)
 
 UNV_PATH = '/'.join((sys.path[-1].split('/')[:5])) + '/data'
 
-#Dataframes
+##########Dataframes##########
+#Projections
 PROJECTIONS_2021 = UNV_PATH + '/2021projections.csv'
+PROJECTIONS_2020 = 'https://raw.githubusercontent.com/fantasydatapros/data/master/fantasypros/fp_projections.csv'
+
+#ADP
+ADP_2020_STANDARD = 'https://raw.githubusercontent.com/fantasydatapros/data/master/fantasypros/adp/STANDARD_ADP.csv'
+ADP_2020_HALF_PPR = 'https://raw.githubusercontent.com/fantasydatapros/data/master/fantasypros/adp/HALF_PPR_ADP.csv'
+ADP_2020_PPR = 'https://raw.githubusercontent.com/fantasydatapros/data/master/fantasypros/adp/PPR_ADP.csv'
+ADP_2021_STANDARD= UNV_PATH + '/2021ADPStd.csv'
+ADP_2021_HALF_PPR= UNV_PATH + '/2021ADPHalfPPR.csv'
+ADP_2021_PPR= UNV_PATH + '2021ADPPPR.csv'
+
+
+##########FantasyScoring##########
+STANDARD_SCORING = {
+    'Receptions': 0,
+    'ReceivingYds': 0.1,
+    'ReceivingTD': 6,
+    'FL': -2,
+    'RushingAtt': 0,
+    'RushingYds': 0.1,
+    'RushingTD': 6,
+    'PassingYds': 0.04,
+    'PassingTD': 4,
+    'Int': -2
+}
+
+HALF_PPR_SCORING = {
+    'Receptions': 0.5,
+    'ReceivingYds': 0.1,
+    'ReceivingTD': 6,
+    'FL': -2,
+    'RushingAtt': 0,
+    'RushingYds': 0.1,
+    'RushingTD': 6,
+    'PassingYds': 0.04,
+    'PassingTD': 4,
+    'Int': -2
+}
+
+PPR_SCORING = {
+    'Receptions': 1,
+    'ReceivingYds': 0.1,
+    'ReceivingTD': 6,
+    'FL': -2,
+    'RushingAtt': 0,
+    'RushingYds': 0.1,
+    'RushingTD': 6,
+    'PassingYds': 0.04,
+    'PassingTD': 4,
+    'Int': -2
+}

c4929d49bd27a77251392065bce0b9c6a869ded7
srtalaie <srt4721@yahoo.com>
2021-09-01 14:57:22 -0700

    data for 2014-2019 works

--- src/utils/GLOBAL.py
+++ src/utils/GLOBAL.py
@@ -7,7 +7,7 @@ UNV_PATH = '/'.join((sys.path[-1].split('/')[:5])) + '/data'
 
 ##########Dataframes##########
 #Machine Learning Cleaned Data 2014-1018
-CLEANED_DATA_2014_2019 = UNV_PATH + '/2014_2019_Weekly_Data.csv'
+CLEANED_DATA_2014_2019 = UNV_PATH + '/2014_2019_weekly_data_cleaned.csv'
 
 #Projections
 PROJECTIONS_2021 = UNV_PATH + '/2021projections.csv'
